4c303d089532a0e64ae3013b3b51430aeb36b80b
Topi Ojala <supertopi@outlook.com>
2016-03-19 23:55:19 +0200

    Tämä on vielä kunnossa!

---
+++ ReactDemo/Scripts/topi.jsx
@@ -0,0 +1,41 @@
+﻿var siskot = ['Tiina', 'Tanja', 'Terhi']
+
+//Container
+var HelloContainer = React.createClass({
+
+    render: function()
+    {
+        return (
+            <div>
+              <HelloWorld name={siskot[0] }/>
+              <HelloWorld name={siskot[1] }/>
+              <HelloWorld name={siskot[2] }/>
+             </div>
+            );
+    }
+})
+
+// Henkilökohtainen tervehdys
+var HelloWorld = React.createClass({
+
+    getInitialState: function() {
+        return {color: 'blue'};
+    },
+    handleClick: function() {
+        this.setState({ color: 'red' });
+    },
+    render: function () {
+
+        var divstyle = {
+            color: this.state.color
+        };
+
+        return (
+            <div onClick={this.handleClick} style={divstyle} > Hello {this.props.name} </div>
+        )
+    }
+
+});
+
+React.render(<HelloContainer/> , document.getElementById('topi'))
+
